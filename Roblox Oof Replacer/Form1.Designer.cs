076064b3e9b9e8a6a61e10e43ad12096cfaf7969
Mr Meowzz <41499772+MrMeowzz@users.noreply.github.com>
2022-07-27 19:44:17 -0700

    Add project files.

---
+++ Roblox Oof Replacer/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Roblox_Oof_Replacer
+{
+    partial class RobloxOofReplacer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RobloxOofReplacer));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.version = new System.Windows.Forms.Label();
+            this.versionToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Replace!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(298, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Reset Settings";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(12, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Pick Roblox Shortcut";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 13.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(12, 115);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(181, 46);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Pick Sound";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(199, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 23);
+            this.label1.TabIndex = 7;
+            this.label1.TextChanged += new System.EventHandler(this.label_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(199, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(280, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Old Oof";
+            this.label2.TextChanged += new System.EventHandler(this.label_TextChanged);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.DefaultExt = "mp3";
+            this.openFileDialog2.Filter = "Ogg files (*.ogg)|*.ogg";
+            this.openFileDialog2.RestoreDirectory = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "lnk";
+            this.openFileDialog1.Filter = "Shortcut files (*.lnk)|*.lnk";
+            this.openFileDialog1.RestoreDirectory = true;
+            // 
+            // version
+            // 
+            this.version.AutoSize = true;
+            this.version.Location = new System.Drawing.Point(12, 9);
+            this.version.Name = "version";
+            this.version.Size = new System.Drawing.Size(37, 15);
+            this.version.TabIndex = 9;
+            this.version.Text = "v1.0.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(395, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "By Mr Meowzz";
+            // 
+            // RobloxOofReplacer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 255);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.version);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "RobloxOofReplacer";
+            this.Text = "Roblox Oof Replacer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RobloxOofReplacer_FormClosing);
+            this.Load += new System.EventHandler(this.RobloxOofReplacer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label version;
+        private System.Windows.Forms.ToolTip versionToolTip;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Roblox Oof Replacer/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Shell32;
+
+namespace Roblox_Oof_Replacer
+{
+    public partial class RobloxOofReplacer : Form
+    {
+        public RobloxOofReplacer()
+        {
+            InitializeComponent();
+        }
+
+        public static string GetShortcutTargetFile(string shortcutFilename)
+        {
+            string pathOnly = System.IO.Path.GetDirectoryName(shortcutFilename);
+            string filenameOnly = System.IO.Path.GetFileName(shortcutFilename);
+
+            Shell shell = new Shell();
+            Folder folder = shell.NameSpace(pathOnly);
+            FolderItem folderItem = folder.ParseName(filenameOnly);
+            if (folderItem != null)
+            {
+                Shell32.ShellLinkObject link = (Shell32.ShellLinkObject)folderItem.GetLink;
+                return link.Path;
+            }
+
+            return string.Empty;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = openFileDialog1.FileName;
+                Properties.Settings.Default.ShortcutFileName = openFileDialog1.FileName;
+            }
+        }
+
+        private void label_TextChanged(object sender, EventArgs e)
+        {
+            Label lbl = (Label)sender;
+            if (lbl.Image != null) return;
+            using (Graphics cg = lbl.CreateGraphics())
+            {
+                SizeF lblsize = new SizeF(lbl.Width, lbl.Height);
+                SizeF textsize = cg.MeasureString(lbl.Text, lbl.Font, lblsize);
+                while (textsize.Width > lblsize.Width - (lblsize.Width * 0.1))
+                {
+                    lbl.Font = new Font(lbl.Font.Name, lbl.Font.Size - 1, lbl.Font.Style);
+                    textsize = cg.MeasureString(lbl.Text, lbl.Font, lblsize);
+                    if (lbl.Font.Size < 6) break;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                label2.Text = openFileDialog2.FileName;
+                Properties.Settings.Default.SoundFileName = openFileDialog2.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(openFileDialog1.FileName))
+            {
+                MessageBox.Show("Choose your Roblox Shortcut!", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            if (Path.GetExtension(openFileDialog1.FileName) != ".lnk")
+            {
+                MessageBox.Show("Shortcut file provided is not a .lnk file!", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            var location = GetShortcutTargetFile(openFileDialog1.FileName);
+            if (location == string.Empty)
+            {
+                MessageBox.Show("Failed to locate target of shortcut!", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            var ooflocation = Path.Combine(location, @"..\", "content", "sounds", "ouch.ogg");
+            if (!File.Exists(ooflocation))
+            {
+                MessageBox.Show("Couldn't find ouch.ogg! Shortcut target may not be Roblox OR the path of the sound may have changed. Your Roblox also might not be on the latest version.", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            var oofname = openFileDialog2.SafeFileName;
+            if (openFileDialog2.FileName == string.Empty)
+            {
+                oofname = "Old Oof";
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://www.dropbox.com/s/8204ehoprc90zvf/uuhhh.ogg?dl=1", ooflocation);
+                }
+            }
+            else
+            {
+                if (!File.Exists(openFileDialog2.FileName))
+                {
+                    MessageBox.Show("Sound file provided does not exist.", "Error", MessageBoxButtons.OK);
+                    return;
+                }
+                MessageBox.Show(Path.GetExtension(openFileDialog2.FileName));
+                if (Path.GetExtension(openFileDialog2.FileName) != ".ogg")
+                {
+                    MessageBox.Show("Sound file provided is not an ogg!", "Error", MessageBoxButtons.OK);
+                    return;
+                }
+                File.Copy(openFileDialog2.FileName, ooflocation, true);
+            }
+            MessageBox.Show("Successfully replaced current oof sound with " + oofname + "!", "Success", MessageBoxButtons.OK);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Reset();
+            openFileDialog1.FileName = Properties.Settings.Default.ShortcutFileName;
+            openFileDialog2.FileName = Properties.Settings.Default.SoundFileName;
+            label1.Text = Properties.Settings.Default.ShortcutFileName;
+            label2.Text = "Old Oof";
+        }
+
+        private void RobloxOofReplacer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void RobloxOofReplacer_Load(object sender, EventArgs e)
+        {
+            version.Text = "v" + System.Windows.Forms.Application.ProductVersion;
+            versionToolTip.SetToolTip(version, "v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            openFileDialog1.FileName = Properties.Settings.Default.ShortcutFileName;
+            openFileDialog2.FileName = Properties.Settings.Default.SoundFileName;
+            label1.Text = Properties.Settings.Default.ShortcutFileName;
+            if (Properties.Settings.Default.SoundFileName == "")
+                label2.Text = "Old Oof";
+            else
+                label2.Text = Properties.Settings.Default.SoundFileName;
+        }
+    }
+}
